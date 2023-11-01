7feeaea03946339b51f3027d5ac108cb41c04265
Labonno Khan Bonna <bonnalabonnokhan2708@gmail.com>
2023-11-01 23:33:12 +0600

    mys second commit

---
+++ arrow.js
@@ -0,0 +1,7 @@
+function add (a,b){
+    const result = a + b;
+    return result;
+}
+
+const sum = add (5,90);
+console.log(sum);
---
+++ default.js
@@ -0,0 +1,17 @@
+function add (num1=99 , num2 = 0){
+    const result = num1 + num2;
+    console.log(num1 , num2, result);
+    return result;
+}
+
+const sum = add(5,7);
+
+function fullName (first, last = ' '){
+    const full = first + ' ' + last;
+    return full;
+}
+
+function friends(numbs = []){
+
+}
+function person = 
