ed82deae271f40c4d7ec357cfc53776d0e10ca27
Labonno Khan Bonna <bonnalabonnokhan2708@gmail.com>
2023-11-02 00:02:30 +0600

    my third commit

--- arrow.js
+++ arrow.js
@@ -5,3 +5,13 @@ function add (a,b){
 
 const sum = add (5,90);
 console.log(sum);
+
+const add2 = function(a, b){
+    return a + b;
+}
+
+// arrow function 
+
+const add3 = (a,b) => a+b;
+
+const suum = add(5,90);
